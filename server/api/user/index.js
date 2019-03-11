7aec9b627e23860e1aceb74da30b29f12509013d
kadamanand86 <35996685+kadamanand86@users.noreply.github.com>
2019-03-11 16:00:00 +0530

    remove permit from user index file (#18)

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -4,11 +4,19 @@ const express = require('express');
 const controller = require('./user.controller');
 const auth = require('../../auth/auth.service');
 
+// const router = express.Router();
+// router.get('/', auth.isAuthenticated, controller.show);//  get all user permit('UserMaster'),
+// router.get('/:id', auth.isAuthenticated, controller.getUser);// user/get
+// router.post('/update', auth.isAuthenticated, permit('UserMaster'), controller.update);//  update user
+// router.post('/', auth.isAuthenticated, permit('UserMaster'), controller.createNewUser);// auth.isAuthenticated, permit('UserMaster'),  add user
+// router.get('/r/role', auth.isAuthenticated, controller.role);//  get all user
+// module.exports = router;
+
 
 const router = express.Router();
 router.get('/', auth.isAuthenticated, controller.show);//  get all user permit('UserMaster'),
 router.get('/:id', auth.isAuthenticated, controller.getUser);// user/get
-router.post('/update', auth.isAuthenticated, permit('UserMaster'), controller.update);//  update user
-router.post('/', auth.isAuthenticated, permit('UserMaster'), controller.createNewUser);// auth.isAuthenticated, permit('UserMaster'),  add user
+router.post('/update', auth.isAuthenticated, controller.update);//  update user
+router.post('/', auth.isAuthenticated, controller.createNewUser);// auth.isAuthenticated, permit('UserMaster'),  add user
 router.get('/r/role', auth.isAuthenticated, controller.role);//  get all user
 module.exports = router;
