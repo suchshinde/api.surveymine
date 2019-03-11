f84951ed89850982cfc2684b9be8caa2ab0710ef
ashishsanap <42666210+ashishsanap@users.noreply.github.com>
2019-03-11 17:53:33 +0530

    configuration of redis database as database 1 (#20)

--- server/config/redis/index.js
+++ server/config/redis/index.js
@@ -3,11 +3,17 @@ import redis from 'redis';
 const redisClient = redis.createClient({ host: process.env.DB_HOST, port: 6379 });
 const log = console;
 redisClient.on('ready', () => {
-  log.log('start redis');
+    log.log('start redis');
 });
 
 redisClient.on('error', (error) => {
-  log.error('Error in Redis', error);
+    log.error('Error in Redis', error);
+});
+redisClient.select(1, (err, res) => {
+    if(err) {
+        log.error(err);
+    }
+    log.log(res);
 });
 
 
